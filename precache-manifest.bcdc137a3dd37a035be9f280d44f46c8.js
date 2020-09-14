9e07cae705a59274dc38bcef0f4f3eefc397b3e9
difo23 <diffozanzan@gmail.com>
2020-09-14 15:22:58 -0400

    first commit

---
+++ precache-manifest.bcdc137a3dd37a035be9f280d44f46c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5ad4739195baad28d86ce5ec198e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29d814f7390a2afc551c",
+    "url": "/static/js/2.22e69f75.chunk.js"
+  },
+  {
+    "revision": "27f1c73f1dc921bc79aa41830d4cd146",
+    "url": "/static/js/2.22e69f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20dd89cd1a4708fc8550",
+    "url": "/static/js/main.aeabf233.chunk.js"
+  },
+  {
+    "revision": "7cdcd8871c456ea27b94",
+    "url": "/static/js/runtime-main.da010e64.js"
+  }
+]);
